6e546748ac79de93bfb2fc6aa81030a6f2cd1669
Minkyu Hwang <Minkyu.Hwang@darkmatter.ae>
2016-07-10 16:10:21 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		EmployeeInfo e = new EmployeeInfo(199);
+		Integer i = new Integer(10);
+		Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(e, i);
+		p1.<EmployeeInfo> printInfo(e);
+		p1.printInfo(e);
+	}
+
+}
+
+class EmployeeInfo {
+	public int rank;
+
+	EmployeeInfo(int rank) {
+		this.rank = rank;
+	}
+}
+
+class Person<T, S> {
+	public T info;
+	public S id;
+
+	Person(T info, S id) {
+		this.info = info;
+		this.id = id;
+	}
+
+	public <U> void printInfo(U info) {
+		System.out.println(info);
+	}
+}
