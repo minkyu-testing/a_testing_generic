c74ce7e6ed12a4ad93a6259f59451ba4eb33ad09
Minkyu Hwang <Minkyu.Hwang@darkmatter.ae>
2016-07-12 12:00:41 +0400

    confind Generic : implements (구현)

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,18 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		Person p1 = new Person(new EmployeeInfo(1));
+		Person p1 = new Person(new EmployeeInfo(10));
 		System.out.println(p1.info.getLevel());
 		//Person<String> p2 = new Person<String>("부장");
 	}
 
 }
 
-abstract class Info {
-	public abstract int getLevel();
+interface Info{
+    int getLevel();
 }
 
-class EmployeeInfo extends Info {
+class EmployeeInfo implements Info {
 	public int rank;
 
 	EmployeeInfo(int rank) {
@@ -35,6 +35,41 @@ class Person<T extends Info> {
 	}
 }
 	
+/*
+public class Main {
+	
+	public static void main(String[] args) {
+		Person p1 = new Person(new EmployeeInfo(1));
+		System.out.println(p1.info.getLevel());
+		//Person<String> p2 = new Person<String>("부장");
+	}
+	
+}
+
+abstract class Info {
+	public abstract int getLevel();
+}
+
+class EmployeeInfo extends Info {
+	public int rank;
+	
+	EmployeeInfo(int rank) {
+		this.rank = rank;
+	}
+	
+	public int getLevel() {
+		return this.rank;
+	}
+}
+
+class Person<T extends Info> {
+	public T info;
+	
+	Person(T info) {
+		this.info = info;
+	}
+}
+*/
 	/*
 	public class Main {
 		
