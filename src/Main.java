b40876431b39cff0b6ebd16983274ea7744f0a55
Minkyu Hwang <Minkyu.Hwang@darkmatter.ae>
2016-07-12 11:57:51 +0400

    confine Generic : extends(상속)

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,70 @@ import java.util.List;
 public class Main {
 
 	public static void main(String[] args) {
-		EmployeeInfo e = new EmployeeInfo(199);
-		Integer i = new Integer(10);
-		Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(e, i);
-		p1.<Integer> printInfo(i);
-		p1.printInfo(e);
-		p1.printInfo(i);
+		Person p1 = new Person(new EmployeeInfo(1));
+		System.out.println(p1.info.getLevel());
+		//Person<String> p2 = new Person<String>("부장");
 	}
 
 }
 
-class EmployeeInfo {
+abstract class Info {
+	public abstract int getLevel();
+}
+
+class EmployeeInfo extends Info {
 	public int rank;
 
 	EmployeeInfo(int rank) {
 		this.rank = rank;
 	}
+
+	public int getLevel() {
+		return this.rank;
+	}
 }
 
-class Person<T, S> {
+class Person<T extends Info> {
 	public T info;
-	public S id;
 
-	Person(T info, S id) {
+	Person(T info) {
 		this.info = info;
-		this.id = id;
-	}
-
-	public <T> void printInfo(T uu) {
-		System.out.println(uu);
 	}
 }
+	
+	/*
+	public class Main {
+		
+		public static void main(String[] args) {
+			EmployeeInfo e = new EmployeeInfo(199);
+			Integer i = new Integer(10);
+			Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(e, i);
+			p1.<Integer> printInfo(i);
+			p1.printInfo(e);
+			p1.printInfo(i);
+		}
+		
+	}
+	
+	class EmployeeInfo {
+		public int rank;
+		
+		EmployeeInfo(int rank) {
+			this.rank = rank;
+		}
+	}
+	
+	class Person<T, S> {
+		public T info;
+		public S id;
+		
+		Person(T info, S id) {
+			this.info = info;
+			this.id = id;
+		}
+		
+		public <T> void printInfo(T uu) {
+			System.out.println(uu);
+		}
+	}
+	*/
